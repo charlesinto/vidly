c2d75ca9d8506bca5aadb5c8b230fa93e9311489
charlesinto <charles.onuorah@yahoo.com>
2020-02-13 00:14:31 +0100

    Connect ApplicationDbContext

--- vidly/Controllers/CustomerController.cs
+++ vidly/Controllers/CustomerController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Web.Mvc;
 using vidly.Models;
 
@@ -10,36 +12,43 @@ namespace vidly.Controllers
     public class CustomerController : Controller
     {
         List<Customer> customers = new List<Customer>();
+        private MyDbContext _context;
         public CustomerController()
         {
-            customers.Add(new Customer(1, "Charles"));
-            customers.Add(new Customer(2, "Lina Onuorah"));
-            customers.Add(new Customer(3, "Ugonma Onuorah"));
-            customers.Add(new Customer(4, "Chioma Odoguw okeson"));
+            _context = new MyDbContext();
+            //initializeCustomers();
         }
         // GET: Customer
         [Route("customers")]
         public ActionResult Index()
         {
-            
+            var customers = _context.customers.Include(c => c.membershipType).ToList();
             return View(customers);
         }
         [Route("customers/edit/{id}")]
         public ActionResult GetCustomer(int id)
         {
-            var found = false;
-            var selectedCustomer = new Customer();
-            foreach(var customer in customers)
-            {
-                if(customer.Id == id)
-                {
-                    found = true;
-                    selectedCustomer = customer;
-                }
-            }
-            if (found)
-                return View(selectedCustomer);
+            //var found = false;
+            var customer = _context.customers.SingleOrDefault(item => item.Id == id);
+            //var selectedCustomer = new Customer();
+            //foreach(var customer in customers)
+            //{
+            //    if(customer.Id == id)
+            //    {
+            //        found = true;
+            //        selectedCustomer = customer;
+            //    }
+            //}
+            if (customer != null)
+                return View(customer);
             return HttpNotFound();
+        }
+        private void initializeCustomers()
+        {
+            customers.Add(new Customer(1, "Charles"));
+            customers.Add(new Customer(2, "Lina Onuorah"));
+            customers.Add(new Customer(3, "Ugonma Onuorah"));
+            customers.Add(new Customer(4, "Chioma Odoguw okeson"));
         }
     }
 }
--- vidly/Models/Customer.cs
+++ vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,6 +13,8 @@ namespace vidly.Models
         public MembershipType membershipType { get; set; }
         public byte MembershipTypeId { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name 
         {
             get

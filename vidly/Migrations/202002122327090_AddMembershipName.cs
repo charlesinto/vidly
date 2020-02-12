b504a1b432e4da68cf6a77213b2d611465d97398
charlesinto <charles.onuorah@yahoo.com>
2020-02-13 00:36:35 +0100

    Add Name to Membership Update Membership Type Database

---
+++ vidly/Migrations/202002122327090_AddMembershipName.cs
@@ -0,0 +1,23 @@
+﻿namespace vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMembershipName : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MembershipTypes", "Name", c => c.String(nullable: false, maxLength: 255));
+            Sql("UPDATE MembershipTypes SET Name = 'Pay As You Go' WHERE Id = 1");
+            Sql("UPDATE MembershipTypes SET Name = 'Month' WHERE Id = 2");
+            Sql("UPDATE MembershipTypes SET Name = 'Quaterly' WHERE Id = 3");
+            Sql("UPDATE MembershipTypes SET Name = 'Yearly' WHERE Id = 4");
+            Sql("UPDATE MembershipTypes SET Name = 'Life Access' WHERE Id = 6");
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.MembershipTypes", "Name");
+        }
+    }
+}
